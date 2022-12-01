d0342d50c7f7bec4b4d3bceab652ed839fb1434c
Berkanozc <berkas12@hotmail.nl>
2022-12-01 12:31:20 +0100

    Add appbar again

--- src/flutter/app/lib/home_page.dart
+++ src/flutter/app/lib/home_page.dart
@@ -2,58 +2,41 @@ import 'package:app/main.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
+  @override
+  State<Home> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<Home> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar("Home"),
       body: Center(
-        child: Text('Homepage'),
+        child: Text("Homepage"),
       ),
-    ); //
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.accessibility_sharp), label: "Current Session"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.history), label: "Sessions History"),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.white,
+        backgroundColor: Colors.deepOrange,
+        onTap: _onItemTapped,
+      ),
+    );
   }
-  @override
-  State<Home> createState() => _MyHomePageState();
-
-
-
-  }
-
-
-class _MyHomePageState extends State<Home> {
-  int _selectedIndex = 0;
-
-
-void _onItemTapped(int index) {
-   setState(() {
-     _selectedIndex = index;
-  });
- }
-
-  @override
-  Widget build(BuildContext context) {
-  return Scaffold(
-  bottomNavigationBar: BottomNavigationBar(
-    type: BottomNavigationBarType.fixed,
-    items: const <BottomNavigationBarItem>[
-      BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-      label: "Home"),
-      BottomNavigationBarItem(
-          icon: Icon(Icons.accessibility_sharp),
-      label: "Current Session"),
-      BottomNavigationBarItem(
-          icon: Icon(Icons.history),
-      label: "Sessions History"),
-  ],
-  currentIndex: _selectedIndex,
-  selectedItemColor: Colors.black,
-  unselectedItemColor: Colors.white,
-  backgroundColor: Colors.deepOrange,
-  onTap: _onItemTapped,
-
-  ),
-  );
 }
-  }
-
