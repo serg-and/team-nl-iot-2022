700a1ecb25de772761a2e40f2b25abb2d63fa447
Rosario Lank <Rosario.lank@hva.nl>
2022-12-05 14:16:21 +0100

    Liam added a current session page, and I connect the current page to the navigationbar.

--- src/flutter/app/lib/data.dart
+++ src/flutter/app/lib/data.dart
@@ -14,11 +14,11 @@ List bottomBar = [
     color: Colors.white,
   ),
   const Icon(
-    Icons.history,
+    Icons.accessibility_sharp,
     color: Colors.white,
   ),
   const Icon(
-    Icons.bluetooth,
+    Icons.history,
     color: Colors.white,
   ),
 ];
