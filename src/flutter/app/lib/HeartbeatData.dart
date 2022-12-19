869af0560ffa31f5c73b189f4ce026289a3db035
andries2 <serge.andriessen@hva.nl>
2022-12-19 13:39:01 +0100

    integrate select script to run on session start into flutter

--- src/flutter/app/lib/HeartbeatData.dart
+++ src/flutter/app/lib/HeartbeatData.dart
@@ -7,7 +7,6 @@ import 'package:fl_chart/fl_chart.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
 
-
 import 'models.dart';
 
 const String SERVER = 'https://team-nl-iot-2022.onrender.com/';
@@ -48,7 +47,7 @@ Future<List<ScriptOutput>> getOutputs(int sessionId) async {
   final sessionOutputs = await supabase
       .from('sessions')
       .select(
-      'script_outputs( id, scripts( id, name, description, output_type, output_name ) )')
+          'script_outputs( id, scripts( id, name, description, output_type, output_name ) )')
       .eq('id', sessionId)
       .single();
 
@@ -70,7 +69,7 @@ Future<List<ScriptOutput>> getOutputs(int sessionId) async {
 }
 
 // start session by opening a websocket
-Future<void> createSession(Function callback) async {
+Future<void> createSession(List<int> scriptIds, Function callback) async {
   IO.Socket socket = IO.io(SERVER, <String, dynamic>{
     'path': '/socket.io',
     'autoConnect': false,
@@ -84,14 +83,22 @@ Future<void> createSession(Function callback) async {
   socket.onConnectError((err) => print('onConnectError: ${err}'));
   socket.onError((err) => print('onError: ${err}'));
 
+  // Temp, select all possible scripts
+  // done because option to select scripts is not yet ready
+  // Generate a list of IDs between 0 and 1000
+  scriptIds = new List<int>.generate(1000, (i) => i);
+
+  // send message to start session with the given scripts
+  socket.emit('start-session', {'scripts': scriptIds});
+
   // session started, start retrieving outputs
   socket.on(
       'sessionId',
-          (sessionId) async => {
-        print('started session: ${sessionId}'),
-        callback(await getOutputs(sessionId)),
-        fakeDataTimer = startSendingFakeData(socket, sessionId),
-      });
+      (sessionId) async => {
+            print('started session: ${sessionId}'),
+            callback(await getOutputs(sessionId)),
+            fakeDataTimer = startSendingFakeData(socket, sessionId),
+          });
 }
 
 class HeartBeatPage extends StatefulWidget {
@@ -107,9 +114,11 @@ class _HeartBeatPageState extends State<HeartBeatPage> {
   @override
   void initState() {
     super.initState();
-    createSession((List<ScriptOutput> sessionOutputs) => setState(() {
-      outputs = sessionOutputs;
-    }));
+    createSession(
+        [],
+        (List<ScriptOutput> sessionOutputs) => setState(() {
+              outputs = sessionOutputs;
+            }));
   }
 
   @override
@@ -148,7 +157,7 @@ class _HeartBeatPageState extends State<HeartBeatPage> {
                     Navigator.pop(context);
                   },
                   style:
-                  ElevatedButton.styleFrom(backgroundColor: Colors.black),
+                      ElevatedButton.styleFrom(backgroundColor: Colors.black),
                   child: Text('Stop session')),
             ),
           )
@@ -176,15 +185,15 @@ class _Output extends State<Output> {
         .stream(primaryKey: ['id'])
         .eq('id', widget.output.id)
         .listen((List<Map<String, dynamic>> data) {
-      if (!data.isEmpty) {
-        setState(() {
-          final int start = data[0]['values'].length - MAX_GRAPH_VALUES;
-          final range = data[0]['values']
-              .getRange(start >= 0 ? start : 0, data[0]['values'].length);
-          values = initOutputValues(range);
+          if (!data.isEmpty) {
+            setState(() {
+              final int start = data[0]['values'].length - MAX_GRAPH_VALUES;
+              final range = data[0]['values']
+                  .getRange(start >= 0 ? start : 0, data[0]['values'].length);
+              values = initOutputValues(range);
+            });
+          }
         });
-      }
-    });
     // add to subscritions so that subscription can be cancled on page dispaose
     subscriptions.add(subscription);
   }
