93123122311355f661ebcd60992e790d8cb4616e
andries2 <serge.andriessen@hva.nl>
2022-12-21 14:59:46 +0100

    Page title in appbar

--- src/flutter/app/lib/main.dart
+++ src/flutter/app/lib/main.dart
@@ -7,15 +7,17 @@ import 'package:app/settings_page.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-Future <void> main() async {
+Future<void> main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
 
   await configureApp(); // Awaiting the configuration of the app
 
-  final prefs = await SharedPreferences.getInstance();// Getting the shared preferences instance
-  final showHome = prefs.getBool('showHome') ?? false;// Getting the value for 'showHome' from the shared preferences, or false if it does not exist
+  final prefs = await SharedPreferences
+      .getInstance(); // Getting the shared preferences instance
+  final showHome = prefs.getBool('showHome') ??
+      false; // Getting the value for 'showHome' from the shared preferences, or false if it does not exist
 
-  runApp(MyApp(showHome: showHome));// Running the MyApp widget
+  runApp(MyApp(showHome: showHome)); // Running the MyApp widget
 }
 
 class MyApp extends StatelessWidget {
@@ -29,7 +31,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: showHome ? Routing() : OnBoardingPage(),// If showHome is true, display the Routing widget, otherwise display the OnBoardingPage
+      home: showHome
+          ? Routing()
+          : OnBoardingPage(), // If showHome is true, display the Routing widget, otherwise display the OnBoardingPage
       debugShowCheckedModeBanner: false, // Don't show the debug banner
     );
   }
@@ -38,31 +42,37 @@ class MyApp extends StatelessWidget {
 class CustomAppBar extends StatelessWidget with PreferredSizeWidget {
   @override
   final Size preferredSize; // The preferred size of the app bar
-  final navigatorKey = GlobalKey<NavigatorState>(); // A key to access the app's navigator
+  final navigatorKey =
+      GlobalKey<NavigatorState>(); // A key to access the app's navigator
   final String title; // The title to display in the app bar
+  final bool showOptions;
 
-  CustomAppBar(this.title, {Key? key})
-      : preferredSize = Size.fromHeight(50.0), // Set the app bar's preferred height to 50.0
+  CustomAppBar(this.title, {Key? key, this.showOptions = true})
+      : preferredSize =
+            Size.fromHeight(50.0), // Set the app bar's preferred height to 50.0
         super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      title: Text('TeamNL'), // Set the app bar's title to 'TeamNL'
+      title: Text(title ?? 'TeamNL'), // Set the app bar's title to 'TeamNL'
       centerTitle: true,
-      backgroundColor: Color(0xFFF59509), // Set the background color to a hex color code
+      backgroundColor:
+          Color(0xFFF59509), // Set the background color to a hex color code
       actions: [
-        PopupMenuButton(
-            itemBuilder: (ctx) => [
-                  const PopupMenuItem(
-                      value: "Settings", child: Text("Settings"))
-                ],// Set the items in the popup menu to a single 'Settings' option
-            onSelected: (result) {
-              if (title != "Settings") { // If the selected item is not 'Settings'
-                Navigator.of(context).push(
-                    MaterialPageRoute(builder: (context) => const Settings()));
-              }
-            })
+        if (showOptions)
+          PopupMenuButton(
+              itemBuilder: (ctx) => [
+                    const PopupMenuItem(
+                        value: "Settings", child: Text("Settings"))
+                  ], // Set the items in the popup menu to a single 'Settings' option
+              onSelected: (result) {
+                if (title != "Settings") {
+                  // If the selected item is not 'Settings'
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const Settings()));
+                }
+              })
       ],
     );
   }
