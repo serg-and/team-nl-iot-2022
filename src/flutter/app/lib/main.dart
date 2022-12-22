02a48660e8e2b52fa7cdcd029e26bd6b2ab4a135
berkanozc <berkas12@hotmail.nl>
2022-12-22 11:12:39 +0100

    Move to main

--- src/flutter/app/lib/main.dart
+++ src/flutter/app/lib/main.dart
@@ -3,17 +3,71 @@ import 'package:app/onboarding_page.dart';
 import 'package:app/routing.dart';
 import 'package:app/settings_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
+import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import 'bluetoothNew/ble/ble_device_connector.dart';
+import 'bluetoothNew/ble/ble_device_interactor.dart';
+import 'bluetoothNew/ble/ble_logger.dart';
+import 'bluetoothNew/ble/ble_scanner.dart';
+import 'bluetoothNew/ble/ble_status_monitor.dart';
+
 Future<void> main() async {
-  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final _ble = FlutterReactiveBle();
+  final _bleLogger = BleLogger(ble: _ble);
+  final _scanner = BleScanner(ble: _ble, logMessage: _bleLogger.addToLog);
+  final _monitor = BleStatusMonitor(_ble);
+  final _connector = BleDeviceConnector(
+    ble: _ble,
+    logMessage: _bleLogger.addToLog,
+  );
+  final _serviceDiscoverer = BleDeviceInteractor(
+    bleDiscoverServices: _ble.discoverServices,
+    readCharacteristic: _ble.readCharacteristic,
+    writeWithResponse: _ble.writeCharacteristicWithResponse,
+    writeWithOutResponse: _ble.writeCharacteristicWithoutResponse,
+    subscribeToCharacteristic: _ble.subscribeToCharacteristic,
+    logMessage: _bleLogger.addToLog,
+  );
 
   await configureApp();
 
   final prefs = await SharedPreferences.getInstance();
   final showHome = prefs.getBool('showHome') ?? false;
 
-  runApp(MyApp(showHome: showHome));
+  runApp(MultiProvider(
+      providers: [
+        Provider.value(value: _scanner),
+        Provider.value(value: _monitor),
+        Provider.value(value: _connector),
+        Provider.value(value: _serviceDiscoverer),
+        Provider.value(value: _bleLogger),
+        StreamProvider<BleScannerState?>(
+          create: (_) => _scanner.state,
+          initialData: const BleScannerState(
+            discoveredDevices: [],
+            scanIsInProgress: false,
+          ),
+        ),
+        StreamProvider<BleStatus?>(
+          create: (_) => _monitor.state,
+          initialData: BleStatus.unknown,
+        ),
+        StreamProvider<ConnectionStateUpdate>(
+          create: (_) => _connector.state,
+          initialData: const ConnectionStateUpdate(
+            deviceId: 'Unknown device',
+            connectionState: DeviceConnectionState.disconnected,
+            failure: null,
+          ),
+        ),
+      ],
+      child: MyApp(
+        showHome: showHome,
+      )));
 }
 
 class MyApp extends StatelessWidget {
@@ -51,11 +105,10 @@ class CustomAppBar extends StatelessWidget with PreferredSizeWidget {
       backgroundColor: Color(0xFFF59509),
       actions: [
         PopupMenuButton(
-            itemBuilder: (ctx) =>
-            [
-              const PopupMenuItem(
-                  value: "Settings", child: Text("Settings"))
-            ],
+            itemBuilder: (ctx) => [
+                  const PopupMenuItem(
+                      value: "Settings", child: Text("Settings"))
+                ],
             onSelected: (result) {
               if (title != "Settings") {
                 Navigator.of(context).push(
