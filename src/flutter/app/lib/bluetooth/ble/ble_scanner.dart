3f6a8ca9f1085b04c4f47b19f5e24a8c1376f18c
berkanozc <berkas12@hotmail.nl>
2023-01-09 17:56:32 +0100

    Format code

--- src/flutter/app/lib/bluetooth/ble/ble_scanner.dart
+++ src/flutter/app/lib/bluetooth/ble/ble_scanner.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:app/bluetooth/ble/reactive_state.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 import 'package:meta/meta.dart';
 
@@ -16,22 +17,23 @@ class BleScanner implements ReactiveState<BleScannerState> {
   final StreamController<BleScannerState> _stateStreamController =
       StreamController();
 
-  final _devices = <DiscoveredDevice>[];
+  final devices = <DiscoveredDevice>[];
+  final connected = <DiscoveredDevice>[];
 
   @override
   Stream<BleScannerState> get state => _stateStreamController.stream;
 
   void startScan(List<Uuid> serviceIds) {
     _logMessage('Start ble discovery');
-    _devices.clear();
+    devices.clear();
     _subscription?.cancel();
     _subscription =
         _ble.scanForDevices(withServices: serviceIds).listen((device) {
-      final knownDeviceIndex = _devices.indexWhere((d) => d.id == device.id);
+      final knownDeviceIndex = devices.indexWhere((d) => d.id == device.id);
       if (knownDeviceIndex >= 0) {
-        _devices[knownDeviceIndex] = device;
+        devices[knownDeviceIndex] = device;
       } else {
-        _devices.add(device);
+        devices.add(device);
       }
       _pushState();
     }, onError: (Object e) => _logMessage('Device scan fails with error: $e'));
@@ -41,7 +43,8 @@ class BleScanner implements ReactiveState<BleScannerState> {
   void _pushState() {
     _stateStreamController.add(
       BleScannerState(
-        discoveredDevices: _devices,
+        connectedDevices: connected,
+        discoveredDevices: devices,
         scanIsInProgress: _subscription != null,
       ),
     );
@@ -65,10 +68,12 @@ class BleScanner implements ReactiveState<BleScannerState> {
 @immutable
 class BleScannerState {
   const BleScannerState({
+    required this.connectedDevices,
     required this.discoveredDevices,
     required this.scanIsInProgress,
   });
 
+  final List<DiscoveredDevice> connectedDevices;
   final List<DiscoveredDevice> discoveredDevices;
   final bool scanIsInProgress;
 }
