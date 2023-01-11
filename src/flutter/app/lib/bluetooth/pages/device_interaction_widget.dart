0250b02f4917a0a299891266c3ce2ba7cc5a8f47
berkanozc <berkas12@hotmail.nl>
2023-01-11 12:32:43 +0100

    Change colors

--- src/flutter/app/lib/bluetooth/pages/device_interaction_widget.dart
+++ src/flutter/app/lib/bluetooth/pages/device_interaction_widget.dart
@@ -128,6 +128,7 @@ class _DeviceInteractionWidgetState extends State<DeviceInteractionWidget> {
         trailing: ElevatedButton(
           child: Text("Get"),
           onPressed: () => deviceModel.getTemperature(),
+          style: ElevatedButton.styleFrom(primary: Colors.deepOrange),
         ),
       ),
     );
