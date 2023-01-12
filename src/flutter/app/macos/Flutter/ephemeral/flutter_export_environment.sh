842ef661d696ff724c2913fd1978063773ac1bc2
berkanozc <berkas12@hotmail.nl>
2023-01-12 11:03:27 +0100

    Add some files

--- src/flutter/app/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ src/flutter/app/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/serge/Library/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/serge/School/dev/team-nl-beachvolley-data-collection/src/flutter/app"
+export "FLUTTER_ROOT=C:\Users\Eigenaar\Documents\Connectors\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Eigenaar\Documents\projects\visualstudiocode\team-nl-beachvolley-data-collection\src\flutter\app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=/Users/serge/School/dev/team-nl-beachvolley-data-collection/src/flutter/app/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
-export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=/Users/serge/School/dev/team-nl-beachvolley-data-collection/src/flutter/app/.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
