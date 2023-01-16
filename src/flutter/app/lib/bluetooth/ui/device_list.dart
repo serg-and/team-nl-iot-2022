2e7e61ff9e8d249e489193a31eafd8c90cd1bff3
andries2 <serge.andriessen@hva.nl>
2023-01-16 13:06:04 +0100

    #documentation add new app features to docs

--- src/flutter/app/lib/bluetooth/ui/device_list.dart
+++ src/flutter/app/lib/bluetooth/ui/device_list.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:filter_list/filter_list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 import 'package:provider/provider.dart';
