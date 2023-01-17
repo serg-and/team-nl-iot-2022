8357c46978977ba9a78c011eed8b92e7f4fa4ee7
andries2 <serge.andriessen@hva.nl>
2023-01-17 11:36:01 +0100

    implement team_members model

--- src/flutter/app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ src/flutter/app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,7 +8,7 @@ import Foundation
 import app_links
 import flutter_secure_storage_macos
 import path_provider_macos
-import shared_preferences_macos
+import shared_preferences_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
